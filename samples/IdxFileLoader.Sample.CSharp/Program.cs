5e2227b6460899721eabdffb5f3af529e8852554
Y56380X <y56380x@users.noreply.github.com>
2023-06-21 18:33:07 +0200

    initial code (version 1.0.0)

---
+++ samples/IdxFileLoader.Sample.CSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright Y56380X https://github.com/Y56380X/IdxFileLoader.
+// Licensed under the MIT License.
+
+using CommunityToolkit.HighPerformance;
+using IdxFileLoader;
+
+await using var idxFile1 = await IdxFile.LoadAsync ("./train-images-idx3-ubyte/train-images.idx3-ubyte");
+await using var idxFile2 = await IdxFile.LoadAsync ("./train-labels-idx1-ubyte/train-labels.idx1-ubyte");
+
+Console.WriteLine($"File 1 is {idxFile1.GetType().Name}");
+Console.WriteLine($"File 2 is {idxFile2.GetType().Name}");
+Console.WriteLine();
+
+if (idxFile1 is IdxImageFile imageFile)
+{
+    Console.WriteLine($"File 1 contains {imageFile.ImageCount} images ({imageFile.ImageSize})");
+    
+    var extractCount = imageFile.ImageCount < 20 ? imageFile.ImageCount : 20; 
+    Console.WriteLine($"Extract {extractCount} images");
+    
+    var buffer = new byte[imageFile.ImageSize.Height, imageFile.ImageSize.Width];
+    for (var i = 0; i < extractCount; i++)
+    {
+        await imageFile.ReadImageBufferAsync(buffer.AsMemory());
+        var image = Image.LoadPixelData<L8>(buffer.AsSpan(), imageFile.ImageSize.Width, imageFile.ImageSize.Height);
+        image.SaveAsPng($"./image_{i + 1}.png");
+    }
+    Console.WriteLine();
+}
+
+if (idxFile2 is IdxLabelFile labelFile)
+{
+    Console.WriteLine($"File 2 contains {labelFile.LabelCount} labels");
+    
+    var extractCount = labelFile.LabelCount < 20 ? labelFile.LabelCount : 20; 
+    Console.WriteLine($"Extract {extractCount} labels");
+    
+    var labels = new byte[extractCount];
+    for (var i = 0; i < extractCount; i++)
+        labels[i] = await labelFile.ReadLabelAsync();
+    Console.WriteLine(string.Join(", ", labels));
+}
---
+++ src/IdxFileLoader/IdxFile.fs
@@ -0,0 +1,83 @@
+﻿// Copyright Y56380X https://github.com/Y56380X/IdxFileLoader.
+// Licensed under the MIT License.
+
+namespace IdxFileLoader
+
+open System
+open System.Drawing
+open System.IO
+open Microsoft.FSharp.Collections
+open Microsoft.FSharp.Core
+
+type internal IdxInfo = { DataType: Type; DimensionCounts: int array }
+
+type IdxFile internal (stream: Stream, info: IdxInfo) =
+    static let loadInfoAsync (stream: Stream) = task {
+        let headerBuffer = Array.zeroCreate 4
+        do! stream.ReadExactlyAsync (headerBuffer.AsMemory ())
+        let header =
+           match headerBuffer with
+           | [|0uy; 0uy; 8uy; dc |] -> {| DataType = typeof<byte>; DimensionCount = int dc |}
+           | [|0uy; 0uy; dt ; _  |] -> failwith $"Data type {dt} currently not supported."
+           | _                      -> failwith "Magic number format error."
+        let countsBuffer = Array.zeroCreate (4 * header.DimensionCount)
+        do! stream.ReadExactlyAsync (countsBuffer.AsMemory ())
+        let msbInt32 (bs: byte Memory) =
+            if BitConverter.IsLittleEndian
+            then bs.Span.Reverse ()
+            BitConverter.ToInt32 bs.Span
+        let dimensionCounts = [|for i in [0..header.DimensionCount - 1] do countsBuffer.AsMemory (i * 4, 4) |> msbInt32 |]
+        return {
+            DataType = header.DataType
+            DimensionCounts = dimensionCounts
+        }
+    }
+    static member LoadAsync (stream: Stream) = task {
+        let! info = loadInfoAsync stream
+        let idxFile : IdxFile =
+            match info.DimensionCounts.Length with
+            | 1 -> new IdxLabelFile (stream, info)
+            | 3 -> new IdxImageFile (stream, info)
+            | _ -> new IdxFile      (stream, info)
+        return idxFile
+    }
+    static member LoadAsync (path: string) = task {
+        let file = File.OpenRead path
+        let! idxFile =
+            try IdxFile.LoadAsync file
+            with e -> file.Dispose ()
+                      reraise ()
+        return idxFile
+    }
+    member val ItemCount  = info.DimensionCounts[0]
+    member val BufferSize = info.DimensionCounts |> Seq.skip 1 |> Seq.fold (*) 1
+    member this.ReadDataSegmentAsync (buffer: byte Memory) = stream.ReadExactlyAsync buffer
+    member this.ReadDataSegmentAsync () = task {
+        let buffer = Array.zeroCreate this.BufferSize
+        do! this.ReadDataSegmentAsync (buffer.AsMemory ())
+        return buffer
+    }
+    interface IDisposable with
+        member this.Dispose() = stream.Dispose ()
+    interface IAsyncDisposable with
+        member this.DisposeAsync() = stream.DisposeAsync ()
+
+and IdxImageFile internal (stream, info) =
+    inherit IdxFile (stream, info)
+    member val ImageCount = info.DimensionCounts[0]
+    member val ImageSize = Size (info.DimensionCounts[2], info.DimensionCounts[1])
+    member this.ReadImageBufferAsync (buffer: byte Memory) = this.ReadDataSegmentAsync buffer
+    member this.ReadImageBufferAsync () = task {
+        let! buffer = this.ReadDataSegmentAsync ()
+        return Array2D.init
+            this.ImageSize.Height this.ImageSize.Width
+            (fun y x -> buffer[y * this.ImageSize.Height + x])
+    }
+
+and IdxLabelFile internal (stream, info) =
+    inherit IdxFile (stream, info)
+    member val LabelCount = info.DimensionCounts[0]
+    member this.ReadLabelAsync () = task {
+        let! buffer = this.ReadDataSegmentAsync ()
+        return buffer |> Seq.exactlyOne
+    }
